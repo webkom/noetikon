637b557975d3c78cc93bd49cd8c42175a0db4a50
Rolf Erik Lekang <me@rolflekang.com>
2015-01-17 11:30:15 +0100

    Add LoginRequiredMixin in directory views

--- noetikon/files/views.py
+++ noetikon/files/views.py
@@ -1,14 +1,15 @@
+from braces.views._access import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 
 from .models import Directory
 
 
-class DirectoryListView(ListView):
+class DirectoryListView(LoginRequiredMixin, ListView):
     model = Directory
 
     def get_queryset(self):
         return Directory.objects.filter(parent_folder=None)
 
 
-class DirectoryDetailView(DetailView):
+class DirectoryDetailView(LoginRequiredMixin, DetailView):
     model = Directory
