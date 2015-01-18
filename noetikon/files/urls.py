dba06a4858d219dcd9b2a4614003b257d511a661
Rolf Erik Lekang <me@rolflekang.com>
2015-01-18 01:46:32 +0100

    pep8

--- noetikon/files/urls.py
+++ noetikon/files/urls.py
@@ -4,6 +4,7 @@ from .views import DirectoryListView, DirectoryDetailView, FileDownloadView
 
 urlpatterns = [
     url(r'^$', DirectoryListView.as_view(), name='directory-list'),
-    url(r'^download/(?P<slug>[a-zA-Z0-9\-/_ ]+)/$', FileDownloadView.as_view(), name='file-download'),
+    url(r'^download/(?P<slug>[a-zA-Z0-9\-/_ ]+)/$', FileDownloadView.as_view(),
+        name='file-download'),
     url(r'^(?P<slug>[a-zA-Z0-9\-/_ ]+)/$', DirectoryDetailView.as_view(), name='directory-detail'),
 ]
