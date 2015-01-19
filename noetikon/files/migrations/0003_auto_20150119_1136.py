75ef284c294fec54292f89661e630f7921cb6b66
Rolf Erik Lekang <me@rolflekang.com>
2015-01-19 14:02:30 +0100

    Use TimestampModel in Directory and File

---
+++ noetikon/files/migrations/0003_auto_20150119_1136.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import basis.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('files', '0002_auto_20150118_0047'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='file',
+            name='created_by',
+        ),
+        migrations.RemoveField(
+            model_name='file',
+            name='updated_by',
+        ),
+        migrations.AddField(
+            model_name='directory',
+            name='created_at',
+            field=models.DateTimeField(editable=False, default=basis.models._now),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='directory',
+            name='updated_at',
+            field=models.DateTimeField(editable=False, default=basis.models._now),
+            preserve_default=True,
+        ),
+    ]
--- noetikon/files/models.py
+++ noetikon/files/models.py
@@ -1,11 +1,12 @@
 import os
+import sys
+from datetime import datetime
 
 from django.conf import settings
 from django.db import models
 from django.utils.functional import cached_property
 
-from basis.models import PersistentModel, BasisModel
-import sys
+from basis.models import PersistentModel, TimeStampModel
 from sorl.thumbnail.shortcuts import get_thumbnail
 
 from noetikon.helpers import slugify
@@ -13,6 +14,10 @@ from .managers import DirectoryManager, FileManager
 
 
 class FilePropertyMixin(object):
+
+    def __str__(self):
+        return self.name
+
     @cached_property
     def name(self):
         if self.exists:
@@ -27,9 +32,12 @@ class FilePropertyMixin(object):
     def exists(self):
         return os.path.exists(self.path)
 
+    @cached_property
+    def modified_time(self):
+        return datetime.fromtimestamp(os.path.getmtime(self.path))
 
 
-class Directory(FilePropertyMixin, PersistentModel):
+class Directory(FilePropertyMixin, TimeStampModel, PersistentModel):
     path = models.TextField(unique=True)
     slug = models.TextField(unique=True, editable=False)
     parent_folder = models.ForeignKey('self', related_name='children', null=True, blank=True)
@@ -41,9 +49,6 @@ class Directory(FilePropertyMixin, PersistentModel):
     class Meta:
         verbose_name_plural = 'directories'
         ordering = ['path']
-
-    def __str__(self):
-        return self.path
 
     def save(self, *args, **kwargs):
         if self.parent_folder is not None:
@@ -75,7 +80,7 @@ class Directory(FilePropertyMixin, PersistentModel):
                     sys.stdout.write('.')
 
 
-class File(FilePropertyMixin, BasisModel):
+class File(FilePropertyMixin, TimeStampModel, PersistentModel):
     path = models.TextField(unique=True)
     slug = models.TextField(unique=True, editable=False)
     parent_folder = models.ForeignKey(Directory, related_name='files')
@@ -84,9 +89,6 @@ class File(FilePropertyMixin, BasisModel):
 
     class Meta:
         ordering = ['path']
-
-    def __str__(self):
-        return self.path
 
     def save(self, *args, **kwargs):
         self.slug = os.path.join(self.parent_folder.slug, slugify(os.path.basename(self.path)))
