0b31d38f4dc1f5ceab3891460ceaa4e78b04e93c
Rolf Erik Lekang <me@rolflekang.com>
2015-01-19 14:36:19 +0100

    Add MEDIA_ROOT in base settings

--- noetikon/settings/base.py
+++ noetikon/settings/base.py
@@ -50,6 +50,7 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../uploads')
 STATIC_URL = '/static/'
 MEDIA_URL = '/uploads/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
