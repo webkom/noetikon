fca5ad946e8a82a0ad82d8d2057e139f5a5085ad
Martin Ek <mail@ekmartin.com>
2016-08-14 18:58:17 +0200

    Ignore specific flake8 lines for import errors

--- noetikon/settings/__init__.py
+++ noetikon/settings/__init__.py
@@ -1,23 +1,23 @@
 # -*- coding: utf8 -*-
 import sys
 
-from noetikon.settings.base import *  # noqa
-from noetikon.settings.noetikon import *  # noqa
-from noetikon.settings.rest_framework import *  # noqa
+from noetikon.settings.base import *  # noqa: F403
+from noetikon.settings.noetikon import *  # noqa: F403
+from noetikon.settings.rest_framework import *  # noqa: F403
 
 try:
-    from noetikon.settings.local import *  # noqa
+    from noetikon.settings.local import *  # noqa: F403
 except ImportError:
     raise ImportError("Couldn't load local settings noetikon.settings.local")
 
-if 'debug_toolbar' in INSTALLED_APPS:
-    from noetikon.settings.debug_toolbar import *  # noqa
+if 'debug_toolbar' in INSTALLED_APPS:  # noqa: F405
+    from noetikon.settings.debug_toolbar import *  # noqa: F403
 
-if 'nopassword' in INSTALLED_APPS:
-    from noetikon.settings.nopassword import *  # noqa
+if 'nopassword' in INSTALLED_APPS:  # noqa: F405
+    from noetikon.settings.nopassword import *  # noqa: F403
 
 if 'test' in sys.argv:
-    from noetikon.settings.test import *  # noqa
+    from noetikon.settings.test import *  # noqa: F403
 
-TEMPLATE_DEBUG = DEBUG
-THUMBNAIL_DEBUG = DEBUG
+TEMPLATE_DEBUG = DEBUG  # noqa: F405
+THUMBNAIL_DEBUG = DEBUG  # noqa: F405
